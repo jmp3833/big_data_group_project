9e41efdf7f8a6c434113640f5a9553fde19bd4de
Justin Peterson <jmp3833@rit.edu>
2015-07-17 19:00:54 +0000

    Abstract Twitter Miner out to a lil' module

---
+++ source/mine_twitter.js
@@ -0,0 +1,14 @@
+/*
+ * Setup Twitter miner to connect to MongoDB and
+ * mine tweets based on importes search criterion.
+ */
+
+var TwitterMiner = require('./twitter_store');
+
+var searchParams = require('./terms.json');
+searchParams = searchParams.join(',');
+var mongoURL = 'mongodb://127.0.0.1:27017/test';
+
+var criterion = {track: searchParams, language: 'en'}
+
+TwitterMiner.streamTweets(mongoURL, criterion);
--- source/twitter_store.js
+++ source/twitter_store.js
@@ -1,7 +1,7 @@
 /*
  * Grab tweets with given keywords and store in a local MongoDB instance.
  * @author: Justin Peterson
- * @date: 07/01/15
+ * @date: 07/15/15
 */
 
 //Twitter API abstraction for NodeJS
@@ -13,43 +13,52 @@ var MongoClient = require('mongodb').MongoClient;
  * in order to reduce redundancies and not write it to the DB
  * @tweetText: <String> text content of a tweet
  */
-function isRetweet(tweetText) {
+function _isRetweet(tweetText) {
   //All retweets begin with 'RT @'
   return tweetText.indexOf("RT @") > -1;
 }
 
-//Any strings you would like to filter the Tweets by.
-var searchParams = require('./terms.json');
-searchParams = searchParams.join(',');
-console.log(searchParams);
+function _connectToDB(url, callback) {
+  MongoClient.connect(url, function(dbConnectionErr, db) {
+    callback(dbConnectionErr, db);
+  });
+}
 
-var twitterAPI = new Twit({
-  //These are specific to the application
-  consumer_key: 'qCi8ZXdOYutLp5zzXOEmgBO6X', 
-  consumer_secret: '9CQfMVqpo5TPibMW18CM2P2OuVGcqvB6hBlGShPREBRSEOtrzF',
+function _connectToTwitterAPI() {
+  var twitterAPI = new Twit({
+    //These are specific to the application
+    consumer_key: 'qCi8ZXdOYutLp5zzXOEmgBO6X', 
+    consumer_secret: '9CQfMVqpo5TPibMW18CM2P2OuVGcqvB6hBlGShPREBRSEOtrzF',
 
-  //These are specific to your Twitter account
-  access_token: '778969536-VLEwtOiWcMlbN4a1sPkG2XMKfvBqGLsF9NndpLSX', 
-  access_token_secret: 'x6bJmx0efmBdSPnL35o6e6hi0emYpo3QNQ9QhGZKvMs9d' 
-});
+    //These are specific to your Twitter account
+    access_token: '778969536-VLEwtOiWcMlbN4a1sPkG2XMKfvBqGLsF9NndpLSX', 
+    access_token_secret: 'x6bJmx0efmBdSPnL35o6e6hi0emYpo3QNQ9QhGZKvMs9d' 
+  });
 
-//TODO: Abstract out MongoDB connection code to its own module
-MongoClient.connect('mongodb://127.0.0.1:27017/test', function(dbConnectionErr, db) {
-  if(dbConnectionErr) throw dbConnectionErr;
-  //Filter all tweets with search key 'programming'
-  var stream = twitterAPI.stream('statuses/filter', { track: searchParams, language: 'en' });
-  stream.on('tweet', function (tweet) {
-    db.createCollection('tweets', function(collectionErr, collection) {
-      if(collectionErr) throw collectionErr;
+  return twitterAPI;
+}
 
-      if(!isRetweet(tweet.text)) {
-        collection.insert(tweet, function(dbWriteErr, docs) {
-          if(dbWriteErr) throw dbWriteErr;
-          console.log("Inserted a Tweet to DB!!");
-          console.log("Tweet text: ", tweet.text);
-         });
-      }
+function streamTweets(dbURL, criterion) {
+  var DB = _connectToDB(dbURL, function(dbConnectionErr, db) {
+    var twitterAPI = _connectToTwitterAPI();
+
+    var stream = twitterAPI.stream('statuses/filter', criterion );
+    stream.on('tweet', function (tweet) {
+      db.createCollection('tweets', function(collectionErr, collection) {
+        if(collectionErr) throw collectionErr;
+
+        if(!_isRetweet(tweet.text)) {
+          collection.insert(tweet, function(dbWriteErr, docs) {
+            if(dbWriteErr) throw dbWriteErr;
+            console.log("Inserted a Tweet to DB!!");
+            console.log("Tweet text: ", tweet.text);
+           });
+        }
+      });
     });
   });
-});
+}
 
+module.exports = {
+  streamTweets: streamTweets
+}

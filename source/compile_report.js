dae80b752771330a23407acfd147e6474e38779b
Justin Peterson <jmp3833@rit.edu>
2015-07-28 22:28:03 +0000

    Hack output together for each of the three sources

--- source/compile_report.js
+++ source/compile_report.js
@@ -2,28 +2,61 @@ var MongoClient = require('mongodb');
 var mongoURL = 'mongodb://127.0.0.1:27017/test';
 var sentiment = require('sentiment');
 var fs = require('fs');
+var rankings = require('./TermParser/languageRankings.json');
 
 MongoClient.connect(mongoURL, function(err, db) {
   if(err) {throw err;}
+
   var ghCursor =  db.collection('github').find({});
   ghCursor.limit(10000);
-  
+  var soCursor =  db.collection('stackexchange').find({});
+  soCursor.limit(10000);
+  var twCursor =  db.collection('tweets').find({});
+  twCursor.limit(10000);
+
   //Build up each issue in CSV format to pipe into rattle.
-  var githubCSV = 'statement,sentiment,language,rating,source\n';
+  var csvHeader = 'statement,sentiment,language,rating,source\n';
+  var githubCSV = '';
+  var twitterCSV = '';
+  var stackexchangeCSV = '';
 
   ghCursor.toArray(function(err, documents) {
+    if(err) {throw err;}
     documents.forEach(function(document) {
-
-      //Strip newlines and special characters
-      var textContent = document.body.replace(/\r?\n|\r/g, ' ').replace(/[^\w\s]/gi, '');
-      var r = sentiment(textContent);
-
-      console.log(r.comparative);
-      githubCSV += (textContent + ',' + 'r.comparative' + ',' + 'javascript' + ',' + '2' + ',' + 'github' + '\n');
+      githubCSV += buildCSVLine(document.body, document.language, 'github');
     });
-    fs.writeFile('./output.csv', githubCSV, function(err) {
+    fs.writeFile('./github.csv', csvHeader + githubCSV, function(err) {
       if(err) {throw err;} 
     });
-    db.close();
   });
+
+  soCursor.toArray(function(err, documents) {
+    if(err) {throw err;}
+    documents.forEach(function(document) {
+      stackexchangeCSV += buildCSVLine(document.title, document.language, 'stackexchange');
+    });
+    fs.writeFile('./stackexchange.csv', csvHeader + stackexchangeCSV, function(err) {
+      if(err) {throw err;} 
+    });
+  });
+
+  twCursor.toArray(function(err, documents) {
+    if(err) {throw err;}
+    documents.forEach(function(document) {
+      twitterCSV += buildCSVLine(document.text, document.language, 'twitter');
+    });
+    fs.writeFile('./twitter.csv', csvHeader + twitterCSV, function(err) {
+      if(err) {throw err;} 
+    });
+  });
+
 });
+
+function buildCSVLine(document, language, source) {
+  //Strip newlines and special characters
+  var textContent = document.replace(/\r?\n|\r/g, ' ').replace(/[^\w\s]/gi, '');
+  var r = sentiment(textContent);
+  var ranking = rankings[document.language]? rankings[document.language] : 18;
+
+  return textContent + ',' + r.comparative + ',' + language + ',' + ranking + ',' + source + '\n';
+}
